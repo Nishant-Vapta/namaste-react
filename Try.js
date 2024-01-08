76d76558f3b2cc867fa3e0e9243b5159184fc21c
Nishant-Vapta <nikvapta@gmail.com>
2024-01-08 21:33:55 +0530

    episode -01

---
+++ Try.js
@@ -0,0 +1,18 @@
+
+/*const arr = ["apple", "banana", "orange", "grapes"];
+
+arr.forEach((value, index) => {
+console.log(index, value);
+})
+*/
+
+let obj = {
+  name : 'Nishant',
+  age : 25,
+  death : 'today'
+};
+
+console. log(obj);
+
+let json = JSON.stringify(obj);
+console.log(json);
